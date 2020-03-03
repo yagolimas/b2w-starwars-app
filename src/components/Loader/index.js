8d02297b90771cd3cd8a4c4487d912fc47879803
Yago Schuenck de Lima <yago.lima@radixeng.com.br>
2020-03-03 13:42:01 -0300

    create Star Wars App

---
+++ src/components/Loader/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+export default class Loader extends Component {
+
+    render() {
+        return <div className="spinner">Loading</div>;
+    }
+}
---
+++ src/components/Planets/index.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+import './styles.css';
+
+import Loader from '../Loader'
+
+export default class Planets extends Component {
+
+    state = {
+        planets: [],
+        isLoaded: false
+    }
+
+    componentDidMount() {
+        this.loadPlanet();
+    }
+
+    loadPlanet = async () => {
+        let randomPlanetId = this.getRandomPlanetId();
+
+        this.setLoading(true);
+
+        const response = await api.get(`planets/${ randomPlanetId }`)
+            .then(this.setLoading(false));
+
+        this.setState({ planets: response.data, isLoaded: !!response });
+    }
+
+    setLoading = isLoaded => this.setState({ isLoaded: isLoaded });
+
+    getRandomPlanetId = () => Math.floor((Math.random() * (60 - 1)) + 1);
+
+    render() {
+        let { isLoaded } = this.state;
+        
+        return (
+            !isLoaded ? <Loader/> :
+                
+                <section className="card">
+                    <h3 className="title">{ this.state.planets.name }</h3>
+                
+                    <div className="content">
+                        <p className="category">
+                            <span className="label">Population:&nbsp;</span> { this.state.planets.population }
+                        </p>
+                        <p className="category">
+                            <span className="label">Climate:&nbsp;</span> { this.state.planets.climate }
+                        </p>
+                        <p className="category">
+                            <span className="label">Terrain:&nbsp;</span> { this.state.planets.terrain }
+                        </p>
+                        <p className="category-films">
+                            <span>Featured in { this.state.planets.films?.length } film(s).</span>
+                        </p>
+                    </div>
+                    <div className="btn-next">
+                        <button className="button" onClick={ this.loadPlanet.bind() }>Next</button>
+                    </div>
+                </section>
+        );
+    }
+}
